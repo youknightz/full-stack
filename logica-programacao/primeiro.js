5c641baab4b13f49f3c8081991758c05eaba671f
youknightz <youknightzsarventino@gmail.com>
2025-11-10 15:11:36 -0300

    feat: adicionando logica de programação

---
+++ logica-programacao/primeiro.js
@@ -0,0 +1,11 @@
+// Perguntando o nome para o usuário
+const userName = prompt("Qual é seu nome?");
+
+//Perguntando qual é a idade
+const userAge = prompt("Qual é sua idade?");
+
+// Imprimindo na tela o resultado
+document.write("Seu nome é: " + "<strong>" + userName + "</strong>" + "<br>");
+
+//Imprimindo na tela a idade
+document.write("Sua idade é: " + "<strong>" + userAge + "</strong>" + " Anos");
---
+++ logica-programacao/segundo.js
@@ -0,0 +1,4 @@
+let numero1 = prompt("Digite um número");
+let numero2 = prompt("Digite outro número")
+
+console.log(numero1 + numero2) 
