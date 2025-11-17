775ff4e3cb37fbe98964be432213c12083ad5c42
youknightz <youknightzsarventino@gmail.com>
2025-11-17 16:31:40 -0300

    negocio chato vey

---
+++ smart-contatos/js/script.js
@@ -0,0 +1,10 @@
+const imputNome = document.getElementById("nome");
+// imputEmail
+// imputTel
+
+function btnCadastrar() {
+    console.log(event);
+   event.preventDefault();
+   console.log(imputNome.value);
+   
+}
