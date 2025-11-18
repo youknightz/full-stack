cbc3076cc354dd89657fe09efd73678a8d9a9170
youknightz <youknightzsarventino@gmail.com>
2025-11-18 15:04:50 -0300

    KKKKKKKKKKK

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,10 +1,33 @@
-const imputNome = document.getElementById("nome");
+const form = document.querySelector("form");
+   
+
+form.addEventListener("submit", function(event) {
+    console.log("bom dia")
+     event.preventDefault();
+})
+
+
+// Validação ddo formulário
+/*
+
+    se o input for vazio, adicionar uma mensagem
+
+*/
+if (inputNome.value == "") {
+    alert("AO MN, vai cadastrar o vazio?");
+    return false;
+}
+
+
+const inputNome = document.getElementById("nome");
+const inputEmail = document.getElementById("email");
+const inputTelefone = document.getElementById("telefone");
 // imputEmail
 // imputTel
 
-function btnCadastrar() {
-    console.log(event);
-   event.preventDefault();
-   console.log(imputNome.value);
-   
+function btnCadastrar(event) {
+    event.preventDefault()
+    console.log("Nome", inputNome.value)
+    console.log("Email", inputEmail.value)
+    console.log("Telefone", inputTelefone.value)
 }
