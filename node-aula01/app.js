bba22566b00ccd7cbb49172aaca23f3cf9b08d8c
youknightz <youknightzsarventino@gmail.com>
2025-11-25 15:00:17 -0300

    Eita que nois ta como? rs

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-
-// Variável com os dados produtos 
+// Variável com os dados produtos
 const produtos = [
-    {id: 1, nome: 'Produto A', preco: 100},
-    {id: 1, nome: 'Produto B', preco: 100},
-    {id: 1, nome: 'Produto C', preco: 100},
+    { id: 1, nome: 'Produto A', preco: 100 },
+    { id: 2, nome: 'Produto B', preco: 150 },
+    { id: 3, nome: 'Produto C', preco: 200 },
+];
+
+
+//Variável de times
+const times = [
+    {id: 1, nome: 'Santos', ano: 1912 },
+    {id: 2, nome: 'Mambu', ano: 1910 },
+    {id: 3, nome: 'Bangu', ano: 1904 },
 ];
 
 
@@ -17,21 +24,25 @@ app.get('/', (req, res) => {
     res.send('Bem vindo ao servidor Node.js');
 });
 
-
-
 // Rota produtos
-app.get ('/produtos', () => {
-    resizeBy.sen(produtos);
+app.get('/produtos', (req, res) => {
+    res.send(produtos);
 });
 
 // Rota home
 app.get('/home', (req, res) => {
-    console.log("Olá: ", __dirname);
+    // console.log("olá: ", __dirname);
     res.sendFile(__dirname + '/public/home.html');
 });
 
+// Rota times
+app.get('/times', (req, res) => {
+    console.log(times)
+    res.send(times);
+});
 
-// Executnado os servidores 
+
+// Executando o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
