b98b6548507f1a54e293f53cc016387e1de9ee9b
youknightz <youknightzsarventino@gmail.com>
2025-11-25 16:32:20 -0300

    fazendo um bagui novo ai

--- node-aula01/app.js
+++ node-aula01/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Middleware para servir arquivos estáticos
+app.use(express.static('public'));
+
 // Variável com os dados produtos
 const produtos = [
     { id: 1, nome: 'Produto A', preco: 100 },
---
+++ node-aula01/public/js/script.js
@@ -0,0 +1,1 @@
+alert("Tenho fome")
