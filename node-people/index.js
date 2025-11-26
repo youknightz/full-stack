7bc54ffc71f9d703ea1e0d77e85ca96d5b683f55
youknightz <youknightzsarventino@gmail.com>
2025-11-26 16:18:38 -0300

    adicionando arquivos

--- node-people/index.js
+++ node-people/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000; // Executar na porta 3000
+const PORT = 4000; // Executar na porta 3000
 
 // mock
 const nomes = [
@@ -9,28 +9,21 @@ const nomes = [
   { id: 3, nome: "Pedro", idade: "56" },
   { id: 4, nome: "Samuel", idade: "45" },
   { id: 5, nome: "Doris", idade: "33" },
-];
+]
 
-
-app.get("/nomes", (req. res) => {
+//Buscando nomes (listaNomes)
+app.get("/listaNomes", (req, res) => {
     console.log(nomes);
     res.send(nomes);
 });
 
 
-
-
 // Rota teste
-app.get ("/teste", (req, res) => {
+app.get("/teste", (req, res) => {
     res.send("API nodePeople está funcionando!");
 });
 
 
-
-//Buscando nomes (listaNomes)
-
-
-
-app.listen(port, () => {
-    console.log(`Servidor rodando no endereço https:localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço https:localhost:${PORT}`);
 });
